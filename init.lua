28e62f978b7181e46814052f60089839b3e06c5a
lex-evergreen <lex.m.evergreen@gmail.com>
2024-09-13 14:44:39 -0600

    Added csharpier as formatter

--- init.lua
+++ init.lua
@@ -672,6 +672,7 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'csharpier',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
@@ -724,6 +725,7 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        cs = { 'csharpier' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
