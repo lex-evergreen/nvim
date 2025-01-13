c973b855235ae4539376c72430e92151cf86180e
lex-evergreen <lex.m.evergreen@gmail.com>
2025-01-13 09:18:41 -0700

    Added gitsigns keymaps

--- init.lua
+++ init.lua
@@ -296,6 +296,7 @@ require('lazy').setup({
           vim.keymap.set(mode, l, r, opts)
         end
 
+        -- Navigation
         map('n', ']c', function()
           if vim.wo.diff then
             vim.cmd.normal { ']c', bang = true }
@@ -311,10 +312,28 @@ require('lazy').setup({
             gitsigns.nav_hunk 'prev'
           end
         end)
-
-        map('n', '<leader>gb', function()
-          gitsigns.toggle_current_line_blame()
-        end)
+        -- Actions
+        map('n', '<leader>hs', gitsigns.stage_hunk, { desc = '[h]unk [s]tage' })
+        map('n', '<leader>hr', gitsigns.reset_hunk, { desc = '[h]unk [r]eset' })
+        map('v', '<leader>hs', function()
+          gitsigns.stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
+        end, { desc = '[h]unk [s]tage' })
+        map('v', '<leader>hr', function()
+          gitsigns.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
+        end, { desc = '[h]unk [r]eset' })
+        map('n', '<leader>hS', gitsigns.stage_buffer, { desc = '[h]unk [S]tage buffer' })
+        map('n', '<leader>hu', gitsigns.undo_stage_hunk, { desc = '[h]unk [u]ndo stage' })
+        map('n', '<leader>hR', gitsigns.reset_buffer, { desc = '[h]unk [R]eset buffer' })
+        map('n', '<leader>hp', gitsigns.preview_hunk, { desc = '[h]unk [p]review' })
+        map('n', '<leader>hb', function()
+          gitsigns.blame_line { full = true }
+        end, { desc = '[h]unk [b]lame line' })
+        map('n', '<leader>tb', gitsigns.toggle_current_line_blame, { desc = '[t]oggle [b]lame' })
+        map('n', '<leader>hd', gitsigns.diffthis, { desc = '[h]unk [d]iff' })
+        map('n', '<leader>hD', function()
+          gitsigns.diffthis '~'
+        end, { desc = '[h]unk [D]iff against last commit' })
+        map('n', '<leader>td', gitsigns.toggle_deleted, { desc = '[t]oggle [d]eleted' })
       end,
     },
   },
@@ -384,6 +403,8 @@ require('lazy').setup({
         { '<leader>w', group = '[W]orkspace' },
         { '<leader>t', group = '[T]oggle' },
         { '<leader>g', group = '[G]it', mode = { 'n', 'v' } },
+        { '<leader>h', group = '[H]unk (gitsigns)' },
+        { '<leader>t', group = '[T]oggle (gitsigns)' },
         { '<leader>b', group = 'Current [b]uffer' },
       },
     },
